b2044e13fdb369d4416f495993b3792594982069
Andy Gardner <andy.gardner@fundamental.net>
2013-04-05 15:57:11 +0200

    Fixing issue directly on master

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleTest
     {
         static void Main(string[] args) {
             Console.WriteLine("Git: Boom!");
-            Console.WriteLine("Click any key to continue...");
+            Console.WriteLine("Hit a key to continue...");
             Console.ReadKey();
         }
     }
