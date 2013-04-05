d76e1c6dbb60123c9515b69ef029314556435840
Andy Gardner <andy.gardner@fundamental.net>
2013-04-05 14:54:41 +0200

    Initial commit of new Console project
    Adding the initial project to the empty repository. It's not the most
    interesting project but something to start with.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Git - Boom!");
+            Console.WriteLine("Click any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
