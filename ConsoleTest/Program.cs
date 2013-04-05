7badb533fd1e81720e44c6d5d73b5ffea7327763
Andy Gardner <andy.gardner@fundamental.net>
2013-04-05 15:44:52 +0200

    Removing unused using statements and editing console output

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleTest
 {
     class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Git - Boom!");
+            Console.WriteLine("Git: Boom!");
             Console.WriteLine("Click any key to continue...");
             Console.ReadKey();
         }
