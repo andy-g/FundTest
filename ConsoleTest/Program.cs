a47a2fd3f8469a2b56aaa42a4aa2bcf7900b4a1f
Andy Gardner <andy.gardner@fundamental.net>
2013-04-12 10:54:11 +0200

    Contributing to chicken feature

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleTest
             Console.WriteLine("Git: Boom, Boom, Boom!");
             Console.WriteLine("Knock Knock?");
             Console.WriteLine("Actually, why did the chicken cross the road");
+            Console.WriteLine("To get to the other side?");
             Console.WriteLine("Click any key to continue...");
             Console.ReadKey();
         }
