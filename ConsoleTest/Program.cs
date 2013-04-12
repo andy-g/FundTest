cac52fed7237d1e0bfe1a88bb406a24b8aea13e8
Andre Homann <andre.homann@fundamental.net>
2013-04-12 08:36:13 +0200

    chicken feature

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleTest
         static void Main(string[] args) {
             Console.WriteLine("Git: Boom, Boom, Boom!");
             Console.WriteLine("Knock Knock?");
+            Console.WriteLine("Actually, why did the chicken cross the road");
             Console.WriteLine("Click any key to continue...");
             Console.ReadKey();
         }
