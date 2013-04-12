1e4001a6724e5929c383fa0e52cd5c898f644e51
Andre Homann <andre.homann@fundamental.net>
2013-04-12 08:33:24 +0200

    Ok, start it again

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleTest
     {
         static void Main(string[] args) {
             Console.WriteLine("Git: Boom, Boom, Boom!");
+            Console.WriteLine("Knock Knock?");
             Console.WriteLine("Click any key to continue...");
             Console.ReadKey();
         }
